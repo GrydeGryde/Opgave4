4088a9ebf8c5e36e95657b300ce8c8f7e704c4da
Alexander Grydgaard <alex043t@edu.easj.dk>
2021-10-05 10:59:50 +0200

    Add project files.

---
+++ ObligatoriskOpgave4/Controllers/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ObligatoriskOpgave4.Manager;
+using opgave1;
+
+namespace ObligatoriskOpgave4.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PlayerController : Controller
+    {
+
+        private readonly PlayerManager _manager = new PlayerManager();
+
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<FootballPlayer> Get(int id)
+        {
+            FootballPlayer player = _manager.GetById(id);
+            return player;
+
+        }
+
+        [HttpPost]
+        public ActionResult<FootballPlayer> Post([FromBody] FootballPlayer value)
+        {
+            _manager.Add(value);
+            return value;
+        }
+
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return _manager.Update(id, value);
+        }
+
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return _manager.Delete(id);
+        }
+    }
+}
---
+++ ObligatoriskOpgave4/Manager/PlayerManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using opgave1;
+
+namespace ObligatoriskOpgave4.Manager
+{
+    public class PlayerManager
+    {
+        private static int _nextId = 1;
+
+        private static readonly List<FootballPlayer> Players = new List<FootballPlayer>
+        {
+            new FootballPlayer() {ID = _nextId++, Name = "Ronaldo", Price = 1000, ShirtNumber = 8}
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Players);
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Players.Find(player => player.ID == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer newPlayer)
+        {
+            newPlayer.ID = _nextId++;
+            Players.Add(newPlayer);
+            return newPlayer;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer player = Players.Find(player1 => player1.ID == id);
+            if (player == null) return null;
+            Players.Remove(player);
+            return player;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer player = Players.Find(player1 => player1.ID == id);
+            if (player == null) return null;
+            if(updates.Name != null ) player.Name = updates.Name;
+            player.Price = updates.Price;
+            if (updates.ShirtNumber != 0) player.ShirtNumber = updates.ShirtNumber;
+            return player;
+        }
+    }
+}
